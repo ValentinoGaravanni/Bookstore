07cc18461b505a4980c36a742b7b4c36108b8a5e
Valentino_Garavanni <987bomber@gmail.com>
2022-06-24 20:47:22 +0300

    Started_to_code

---
+++ src/main/java/com/example/demospring/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demospring.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ResponseEntity main (){
+        return ResponseEntity.ok("Well done");
+    }
+
+}
---
+++ src/main/java/com/example/demospring/entity/User.java
@@ -0,0 +1,80 @@
+package com.example.demospring.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Size(min = 2, message = "Not enough, more symbols are needed")
+    private String username;
+    @Size(min = 2 , message = "Username is too short")
+    private String password;
+    @Transient
+    private String passwordConfirm;
+    @Email
+    private String email;
+
+    @ElementCollection(targetClass = Role.class,fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
